eb3c907fa0742c254d01de7368abcb923f1c426b
Manuel Garay <manolinga23@outlook.com>
2026-01-19 05:23:15 -0600

    Added a couple tests

---
+++ test_main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+os.environ["DB_PATH"] = "test.db"
+os.environ["JSON_PATH"] = "data/models.json"
+
+
+client = TestClient(app)
+
+def test_get_brands_check_acura():
+    response = client.get("/brands")
+    assert response.status_code == 200
+    data = response.json()
+    
+    # Find Acura in the list
+    acura = next((b for b in data if b["name"] == "Acura"), None)
+    assert acura is not None
+    assert acura["average_price"] == 702109.5
